c31077be56e02740e1f8e9d5252947ba6fa83672
Aniket Harde <aniket.harde@gemsessence.com>
2022-07-08 19:08:00 +0530

    Added customer mobile number validations

--- app/models/milkman_customer.rb
+++ app/models/milkman_customer.rb
@@ -1,7 +1,7 @@
 class MilkmanCustomer < ApplicationRecord
   validates :customer_name, uniqueness: true
   validates_presence_of :customer_name, :customer_address, :customer_mobile_number
-
+  validates :customer_mobile_number, :numericality => true, :length => { :minimum => 10, :maximum => 15 }
   belongs_to :milkman
   has_many :milkman_customer_products
   has_many :customer_delivery_preferences
